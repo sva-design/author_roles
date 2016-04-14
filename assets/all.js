4e3638d919f8dc3201c3e3093a0e6f9cbec9db58
Justin Wang <justinjaywang@gmail.com>
2016-04-14 12:24:34 -0400

    add support for browsers that don't support required

--- assets/all.js
+++ assets/all.js
@@ -1,9 +1,21 @@
 jQuery(function(){
   var $ = jQuery;
 
-  var $requiredInputs = $('.required').find('input');
-  $requiredInputs.each(function() {
+  var $req = $('.required').find(':input'); // find inputs, textareas, selects
+  $req.each(function() {
     $(this).attr('required', 'required');
+  });
+
+  // for browsers that do not support required attribute
+  $('form').submit(function(e) {
+    $(this).find('[required]').each(function() {
+      if ($(this).val() == '') {
+        e.preventDefault();
+        alert('Please fill out all required fields.');
+        $(this).focus();
+        return false;
+      }
+    });
   });
 
 });
